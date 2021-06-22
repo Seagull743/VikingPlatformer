ad5331d2ef1adfc642849cf59eb869d141472674
Sean <seangullery@gmail.com>
2021-06-22 15:21:02 +1200

    hi

--- 2D Viking Platformer/Assets/Scripts/Seans Scripts/Player2Movement.cs
+++ 2D Viking Platformer/Assets/Scripts/Seans Scripts/Player2Movement.cs
@@ -7,29 +7,20 @@ public class Player2Movement : MonoBehaviour
 
     [SerializeField]
     private float moveSpeed = 5;
-    [SerializeField]
-    private float JumpForce = 5;
 
-    private Rigidbody2D rb;
+  
 
-
-    // Start is called before the first frame update
+   // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void FixedUpdate()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-
-        }
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
+
+    
 }
