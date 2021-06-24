fe60709cf363e46ab067be1072323eb1ac8b3d3d
Sean <seangullery@gmail.com>
2021-06-24 12:11:02 +1200

    Scripts

---
+++ 2D Viking Platformer/Assets/Scripts/Seans Scripts/Interactive.cs
@@ -0,0 +1,64 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactive : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform grabDetect;
+    [SerializeField]
+    private Transform holdLocation;
+    [SerializeField]
+    private float raydist;
+    [SerializeField]
+    private KeyCode interact;
+    [SerializeField]
+    private KeyCode throwing;
+
+    private bool isHolding = false;
+
+    [SerializeField]
+    private float throwforce;
+
+    private RaycastHit2D grabcheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(interact))
+        {
+            if (!isHolding)
+            {
+                grabcheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, raydist);
+                if (grabcheck.collider != null && grabcheck.collider.tag == "Box")
+                {         
+                    grabcheck.collider.gameObject.transform.parent = holdLocation;
+                    grabcheck.collider.gameObject.transform.position = holdLocation.position;
+                    grabcheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                    isHolding = true;
+                }           
+            }
+            else if (isHolding)
+            {
+                grabcheck.collider.gameObject.transform.parent = null;
+                grabcheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                isHolding = false;  
+            }
+        }
+        if (Input.GetKeyDown(throwing) && isHolding)
+        {
+            grabcheck.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 1) * throwforce;
+            grabcheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            grabcheck.collider.gameObject.transform.parent = null;
+        }
+    }
+}
---
+++ 2D Viking Platformer/Assets/Scripts/Seans Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour
+{
+
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private KeyCode left;
+    [SerializeField]
+    private KeyCode right;
+    [SerializeField]
+    private KeyCode jump;
+
+    [SerializeField]
+    private Transform groundChecker;
+    [SerializeField]
+    private LayerMask ground;
+    [SerializeField]
+    private float groundCheckRadius;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundChecker.position, groundCheckRadius, ground);
+
+
+        if (Input.GetKey(left))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+      
+        }
+        else if (Input.GetKey(right))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(jump) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    
+        if(rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-0.3375f, 0.725f, 1);
+        }
+        else if(rb.velocity.x > 0)
+        {
+           transform.localScale = new Vector3(0.3375f, 0.725f, 1);
+        }
+    }
+
+
+    
+}
