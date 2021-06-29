c6ec51b28270aed670e6d108816a440eb98ce75c
Sean <seangullery@gmail.com>
2021-06-29 15:26:11 +1200

    HealthScript && Meter Etc

--- 2D Viking Platformer/Assets/Scripts/Seans Scripts/NextLevel.cs
+++ 2D Viking Platformer/Assets/Scripts/Seans Scripts/NextLevel.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NextLevel : MonoBehaviour
-{
+{   
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+      if(collision.gameObject.tag == "Player")
         {
-            Debug.Log("Next Level...");
+            Debug.Log("Next Level");
         }
+     
     }
 }
---
+++ 2D Viking Platformer/Assets/Scripts/Seans Scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private bool playerAlive = true;
+
+
+
+    public void PlayerDamaged()
+    {
+        playerAlive = false;
+        this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0,10,0);
+        this.gameObject.GetComponent<Interactive>().enabled = false;
+        this.gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+        this.gameObject.GetComponent<PlayerController>().enabled = false;
+        Invoke("ResetGame", 2);
+    }
+
+
+    private void ResetGame()
+    {
+        Debug.Log("Game Over");
+    }
+
+}
