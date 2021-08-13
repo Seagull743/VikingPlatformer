7fb87de5b2eefc4485540b46484e68b4d826a087
Sean <seangullery@gmail.com>
2021-08-13 12:13:32 +1200

    Camera Update

---
+++ 2D Viking Platformer/Assets/Scripts/Seans Scripts/camZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class camZone : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineVirtualCamera virtualCamera = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        virtualCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
