74c1b93bc02cab0b2bf11f7234c7300725d60b39
Sean <seangullery@gmail.com>
2021-09-03 14:19:48 +1200

    Sean updating scripts

---
+++ 2D Viking Platformer/Assets/Scripts/Seans Scripts/DoorPressure.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorPressure : MonoBehaviour
+{
+    private Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.GetComponent<Animator>();
+    }
+
+
+
+   public void OpenDoor()
+    {
+        anim.SetBool("isOpen", true);
+    }
+
+   public void CloseDoor()
+    {
+        anim.SetBool("isOpen", false);
+    }
+
+}
