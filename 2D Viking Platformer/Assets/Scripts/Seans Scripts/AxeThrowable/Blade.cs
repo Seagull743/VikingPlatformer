8ce332bfc4b8a6cc2cae5ec69e1b8588bf12c07d
Sean <seangullery@gmail.com>
2021-10-08 12:16:54 +1300

    Ice platform and one button interaction

---
+++ 2D Viking Platformer/Assets/Scripts/Seans Scripts/AxeThrowable/Blade.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+
+    private Axe axe;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        axe = GetComponentInParent<Axe>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" && axe.thrown)
+        {
+            if (collision.gameObject.GetComponent<MeleeDude>() != null)
+            {
+                collision.gameObject.GetComponent<MeleeDude>().EnemyDieing();
+                axe.AxeStop();
+            }
+            else if (collision.gameObject.GetComponent<Archer>() != null)
+            {
+                collision.gameObject.GetComponent<Archer>().EnemyDieing();
+                axe.AxeStop();
+            }
+        }
+        else if (collision.gameObject.tag != "Enemy" && axe.thrown)
+        {
+            axe.ThrownToggle();
+        }
+    }
+
+}
