da889c7ebec4065ac1ec30575a3b529aa3e9aafd
Dan <DucatiDan@outlook.com>
2021-10-15 14:33:54 +1300

    Dialog for the Raven & Axe Level in Progress
    Raven dialog working but needs speech bubble

---
+++ 2D Viking Platformer/Assets/Dialog.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+
+
+    public void TypeStarter()
+    {
+        StartCoroutine(Type());
+        Invoke("DeleteText", 13);
+    }
+
+    IEnumerator Type()
+    {
+
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void DeleteText()
+    {
+        textDisplay.text = null;
+    }
+
+}
+
---
+++ 2D Viking Platformer/Assets/DialogTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Dialog DialogCanvas;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            DialogCanvas.TypeStarter();
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        } 
+    }
+
+
+
+}
