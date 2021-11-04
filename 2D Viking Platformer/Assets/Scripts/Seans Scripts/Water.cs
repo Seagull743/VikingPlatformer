ba2480b1dbb11ec9fc8a9ea017d97ba8c428d212
Sean <seangullery@gmail.com>
2021-11-04 15:07:55 +1300

    Working on water

---
+++ 2D Viking Platformer/Assets/Scripts/Seans Scripts/Water.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject WaterSplash;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Transform feet = collision.gameObject.GetComponent<PlayerHealth>().BifrostLocation;
+            Instantiate(WaterSplash, feet.position, feet.transform.rotation);
+        }
+    }
+}
