cc92e82cf344d9faaed7eccff76b164af4633cd4
Sean <seangullery@gmail.com>
2021-11-17 10:12:28 +1300

    Added Instructions fixed portal

---
+++ 2D Viking Platformer/Assets/triggerResetText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerResetText : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject instructionReset;
+    private static bool calledReset = false;
+
+    private void Start()
+    {
+        instructionReset.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Box" && !calledReset)
+        {
+            instructionReset.SetActive(true);
+        }
+    }
+
+}
