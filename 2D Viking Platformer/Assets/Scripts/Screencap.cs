456d1d431c0943de464397c5dc98a80b5c7b78af
Dan <DucatiDan@outlook.com>
2021-11-30 13:36:45 +1300

    ScreenCap Script added and new screenshots folder
    Can Screenshot scenes now with Screencap.cs

---
+++ 2D Viking Platformer/Assets/Scripts/Screencap.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screencap : MonoBehaviour
+{
+    //Declaration for file type. Modify with ,[file type for additional types you wish to add]
+    public enum fileType { png, jpg, tif}
+    public fileType targetType = fileType.png;
+
+    //name of folder to ad screenshots to
+    [SerializeField]
+    string User = "";
+    string folderPath = "";
+
+    //Specific key to press to take screenshot
+    public KeyCode targetKey = KeyCode.LeftControl;
+    
+    //Int value for uprezzing captions where 1 is screensize, 2 is double screen size etc.
+    [SerializeField]
+    int size = 1;
+
+    private void Awake()
+    {
+        folderPath = ($"Screenshots/{User}/");
+    }
+
+
+    void Start()
+    {
+        if (!System.IO.Directory.Exists(folderPath))
+            System.IO.Directory.CreateDirectory(folderPath);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(targetKey))
+        {
+            TakeCaption();
+        }
+    }
+
+    void TakeCaption()
+    {
+        var screenShotName = ($"Screenshot_{System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss")}.{targetType}");
+        ScreenCapture.CaptureScreenshot(System.IO.Path.Combine(folderPath, screenShotName), size);
+        Debug.Log(folderPath + screenShotName);
+    }
+}
